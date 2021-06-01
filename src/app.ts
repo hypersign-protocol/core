1d6d7fe02aad4b8e2462ab7ebc5beeb44b0c3f29
vishwas anand <vishwasanand.cse@gmail.com>
2021-06-01 12:36:54 +0530

    removed ratelimiting

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,15 @@ import cors from 'cors';
 
 const app = express();
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: 'Too many requests' // message to send
-  });
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//     message: 'Too many requests' // message to send
+//   });
   
 
 app.use(helmet());
-app.use(limiter);
+// app.use(limiter);
 app.use(xss());
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
